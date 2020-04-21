b887cbf1ff9c7a8316bf9c81ab6b6d35ffa16b7e
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-04-21 18:08:14 +0900

    Update PWA config

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,15 +112,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `筋肉ﾁｮｯﾄﾃﾞｷﾙ`,
+        short_name: `筋肉ﾁｮｯﾄﾃﾞｷﾙ`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/bodybuilding.jpg`,
       },
     },
+    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
@@ -128,8 +129,5 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 };
