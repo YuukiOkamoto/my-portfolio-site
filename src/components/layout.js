c2357c53293dd2ad3d30551e68271c196c33bcce
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-04-25 23:17:31 +0900

    Add reset css

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,22 +3,26 @@ import { css } from '@emotion/core';
 
 import { rhythm } from '../utils/typography';
 
+import ResetCSS from './reset-css';
 import Header from './header';
 import Footer from './footer';
 
 const Layout = ({ location, children }) => (
-  <div
-    css={css`
-      margin-left: auto;
-      margin-right: auto;
-      max-width: ${rhythm(32)};
-      padding: ${rhythm(1.5)} ${rhythm(3 / 4)};
-    `}
-  >
-    <Header location={location} />
-    <main>{children}</main>
-    <Footer />
-  </div>
+  <>
+    <ResetCSS />
+    <div
+      css={css`
+        margin-left: auto;
+        margin-right: auto;
+        max-width: ${rhythm(32)};
+        padding: ${rhythm(1.5)} ${rhythm(3 / 4)};
+      `}
+    >
+      <Header location={location} />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  </>
 );
 
 export default Layout;
