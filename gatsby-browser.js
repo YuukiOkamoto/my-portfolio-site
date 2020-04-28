cd23217da4eb547481fb6a1133c23181879646bd
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-04-28 11:05:10 +0900

    TypographyStyleを優先する

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,18 @@
-// custom typefaces
+import React from 'react';
+import { ThemeProvider, ColorModeProvider, CSSReset } from '@chakra-ui/core';
+import { TypographyStyle } from 'react-typography';
+import typography from './src/utils/typography';
 import 'typeface-montserrat';
 import 'typeface-merriweather';
 
 import 'prismjs/themes/prism-tomorrow.css';
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider>
+    <ColorModeProvider>
+      <CSSReset />
+      <TypographyStyle typography={typography} />
+      {element}
+    </ColorModeProvider>
+  </ThemeProvider>
+);
