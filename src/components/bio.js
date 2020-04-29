058c68455fc84a84c72526635dbf7ca2844b1ec2
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-04-29 20:16:47 +0900

    ChakraUIに置き換え bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,14 +1,17 @@
-/**
- * Bio component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Image from 'gatsby-image';
 import { css } from '@emotion/core';
+import {
+  Avatar,
+  Box,
+  Flex,
+  Link,
+  List,
+  ListItem,
+  PseudoBox,
+  Text,
+} from '@chakra-ui/core';
 
 import { FaTwitter, FaGithub } from 'react-icons/fa';
 
@@ -17,7 +20,7 @@ const Bio = () => {
     query BioQuery {
       avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
         childImageSharp {
-          fixed(width: 50, height: 50) {
+          fixed(width: 80, height: 80) {
             ...GatsbyImageSharpFixed
           }
         }
@@ -39,17 +42,8 @@ const Bio = () => {
 
   const { author, snsAccounts } = data.site.siteMetadata;
   return (
-    <div
-      css={css`
-        display: flex;
-      `}
-    >
-      <div
-        css={css`
-          display: flex;
-          align-items: center;
-        `}
-      >
+    <Flex>
+      <Flex align='center'>
         <Image
           fixed={data.avatar.childImageSharp.fixed}
           alt={author.name}
@@ -63,66 +57,44 @@ const Bio = () => {
             borderRadius: `50%`,
           }}
         />
-      </div>
-      <div
-        css={css`
-          flex: 1;
-          margin-left: 1rem;
-        `}
-      >
-        <div
-          css={css`
-            font-weight: bold;
-          `}
-        >
+      </Flex>
+      <Box flex='1' ml='4'>
+        <Text fontSize='lg' fontWeight='bold'>
           {author.name}
-        </div>
-        <div>{author.summary}</div>
+        </Text>
+        <Text fontSize='sm'>{author.summary}</Text>
         <SnsAccountList snsAccounts={snsAccounts} />
-      </div>
-    </div>
+      </Box>
+    </Flex>
   );
 };
 
-const SnsAccountList = snsAccounts => {
-  const icon = css`
-    margin-right: 10px;
-    &:hover {
-      color: hsl(0, 0%, 50%);
-    }
-  `;
-
-  return (
-    <ul
-      css={css`
-        display: flex;
-        flex-wrap: wrap;
-        list-style: none;
-        margin: 0;
-      `}
-    >
-      <li>
-        <a
-          css={icon}
+const SnsAccountList = snsAccounts => (
+    <List display='flex' flexWrap='wrap' m='0'>
+      <ListItem mr='1'>
+        <Link
           href={`https://twitter.com/${snsAccounts.twitter}`}
-          target='_blank'
-          rel='noopener noreferrer'
+          isExternal
         >
-          <FaTwitter />
-        </a>
-      </li>
-      <li>
-        <a
-          css={icon}
-          href={`https://github.com/${snsAccounts.github}`}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <FaGithub />
-        </a>
-      </li>
-    </ul>
+          <PseudoBox
+            as={FaTwitter}
+            size='4'
+            color='gray.400'
+            _hover={{ color: 'blue.500' }}
+          />
+        </Link>
+      </ListItem>
+      <ListItem mr='1'>
+        <Link href={`https://github.com/${snsAccounts.github}`} isExternal>
+          <PseudoBox
+            as={FaGithub}
+            size='4'
+            color='gray.400'
+            _hover={{ color: 'gray.500' }}
+          />
+        </Link>
+      </ListItem>
+    </List>
   );
-};
 
 export default Bio;
