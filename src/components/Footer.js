1815ec75574945942b5c3a96e45651892ecaf5fd
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-05-04 15:52:11 +0900

    footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Box, Link } from '@chakra-ui/core';
+import { Box, Icon, Link, Stack, Text } from '@chakra-ui/core';
+import { MdCopyright } from 'react-icons/md';
 
 import Container from './Container';
 
 const Footer = () => (
-  <Box as='footer'>
+  <Box as='footer' py='1'>
     <Container>
-      © {new Date().getFullYear()}, Built with
-      {` `}
-      <Link href='https://www.gatsbyjs.org' isExternal>
-        Gatsby
-      </Link>
+      <Stack isInline spacing='4' justify='center'>
+        <Text fontSize='xs'>
+          Made with{` `}
+          <Link href='https://www.gatsbyjs.org/' isExternal>
+            Gatsby
+          </Link>
+          &{' '}
+          <Link href='https://chakra-ui.com/' isExternal>
+            ChakraUi
+          </Link>
+        </Text>
+        <Text fontSize='xs'>
+          <Icon transform='translateY(-2px)' as={MdCopyright} />{' '}
+          Yuuki Okamoto &bull; {new Date().getFullYear()}
+        </Text>
+      </Stack>
     </Container>
   </Box>
 );
