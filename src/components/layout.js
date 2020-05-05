6cb18b26f43af9c1bce1e897197de2c874afeeaf
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-05-05 17:08:03 +0900

    Fix bg color

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Box, Flex } from '@chakra-ui/core';
+import {useColorMode, Box, Flex } from '@chakra-ui/core';
 
 import Header from './Header';
 import Footer from './Footer';
 
 const Layout = ({ location, children }) => {
+   const { colorMode } = useColorMode();
   const rootPath = `${__PATH_PREFIX__}/`;
   const isHome = location.pathname === rootPath;
+  const bg = {light: 'white', dark: 'gray.800'}
 
   return (
-    <Flex direction='column' minH='100vh' mx='auto'>
+    <Flex direction='column' minH='100vh' mx='auto' bg={bg[colorMode]}>
       <Header isHome={isHome} />
       <Box as='main' flex='1'>
         {children}
