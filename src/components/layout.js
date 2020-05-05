0c09209c86dbdf17b706ab0b4a414ec6a7e93a58
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-05-05 17:08:57 +0900

    clean

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {useColorMode, Box, Flex } from '@chakra-ui/core';
+import { useColorMode, Box, Flex } from '@chakra-ui/core';
 
 import Header from './Header';
 import Footer from './Footer';
 
 const Layout = ({ location, children }) => {
-   const { colorMode } = useColorMode();
+  const { colorMode } = useColorMode();
   const rootPath = `${__PATH_PREFIX__}/`;
   const isHome = location.pathname === rootPath;
-  const bg = {light: 'white', dark: 'gray.800'}
+  const bg = { light: 'white', dark: 'gray.800' };
 
   return (
     <Flex direction='column' minH='100vh' mx='auto' bg={bg[colorMode]}>
