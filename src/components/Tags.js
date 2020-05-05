2cb3ed3133027427822218c64419a2f372709dbf
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-05-05 10:20:03 +0900

    Add hover action to Tag

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link as GatsbyLink } from 'gatsby';
 import { GiMuscleUp } from 'react-icons/gi';
-import { Flex, Tag, TagIcon, TagLabel } from '@chakra-ui/core';
+import { Flex, Tag as ChakraTag, TagIcon, TagLabel } from '@chakra-ui/core';
 
 import kebabCase from 'lodash/kebabCase';
 
+const Tag = ({ tag, tagColor }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <ChakraTag
+      as={GatsbyLink}
+      to={`tags/${kebabCase(tag)}`}
+      fontFamily='heading'
+      variant={isHovered ? 'solid' : 'outline'}
+      variantColor={tagColor}
+      ml={[0, 0, 2]}
+      mr={[2, 2, 0]}
+      mb='2'
+      transition='.1s'
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <TagIcon icon={GiMuscleUp} />
+      <TagLabel>{tag}</TagLabel>
+    </ChakraTag>
+  );
+};
 const Tags = ({ post, fontSize, tagColor }) => {
   const { tags } = post.frontmatter;
 
@@ -19,20 +41,7 @@ const Tags = ({ post, fontSize, tagColor }) => {
       fontSize={fontSize}
     >
       {tags.map((tag, i) => (
-        <Tag
-          key={i}
-          as={GatsbyLink}
-          to={`tags/${kebabCase(tag)}`}
-          fontFamily='heading'
-          variant='outline'
-          variantColor={tagColor}
-          ml={[0, 0, 2]}
-          mr={[2, 2, 0]}
-          mb='2'
-        >
-          <TagIcon icon={GiMuscleUp} />
-          <TagLabel>{tag}</TagLabel>
-        </Tag>
+        <Tag key={i} tag={tag} tagColor={tagColor} />
       ))}
     </Flex>
   );
