543f1eb4d89896ef2b6febf0e875d384f837e503
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-05-09 21:46:02 +0900

    Change Twitter card

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -55,7 +55,7 @@ const SEO = ({ description, lang, meta, title }) => {
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary_large_image`,
         },
         {
           name: `twitter:creator`,
