d2a0ffa34eed6fafac282aee2f1a741b25001f4c
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-05-12 21:06:08 +0900

    Change mdx component styles

--- src/components/docsComponents/Callout.js
+++ src/components/docsComponents/Callout.js
@@ -1,7 +1,7 @@
 import React, { useLayoutEffect, useState } from 'react';
 import { useColorMode, Stack, Text } from '@chakra-ui/core';
 
-const Callout = ({ emoji, children, ...props }) => {
+const Callout = ({ emoji, emojiSize ='xl', children, ...props }) => {
   const { colorMode } = useColorMode();
 
   // Note: Workaround this bug https://github.com/YuukiOkamoto/my-blog/issues/14
@@ -16,14 +16,15 @@ const Callout = ({ emoji, children, ...props }) => {
       d='inline-flex'
       spacing='3'
       align='center'
-      bg={{ light: 'gray.100', dark: 'gray.700' }[colorMode]}
-      py='2'
-      px='4'
+      bg={{ light: 'gray.50', dark: 'whiteAlpha.50' }[colorMode]}
+      fontSize='sm'
+      py='4'
+      px='6'
       borderRadius='lg'
       visibility={mounted ? 'visible' : 'hidden'}
       {...props}
     >
-      <Text fontSize='3xl'>{emoji}</Text>
+      <Text fontSize={emojiSize}>{emoji}</Text>
       <Text wordBreak='break-word'>{children}</Text>
     </Stack>
   );
--- src/components/docsComponents/MDXComponents.js
+++ src/components/docsComponents/MDXComponents.js
@@ -17,7 +17,7 @@ import Table, { THead, TData } from './Table';
 
 const MDXComponents = {
   h2: props => <Heading as='h2' size='xl' mt='12' mb='5' {...props} />,
-  h3: props => <Heading as='h3' size='lg' mt='9' mb='4' {...props} />,
+  h3: props => <Heading as='h3' size='lg' mt='10' mb='4' {...props} />,
   h4: props => <Heading as='h4' size='md' mt='6' mb='3' {...props} />,
   h5: props => <Heading as='h5' size='sm' mt='3' mb='2' {...props} />,
   h6: props => <Heading as='h6' size='xs' mt='1' mb='1' {...props} />,
