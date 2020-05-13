7b1be77eeae69a15aabf8ab5299d906496c11efe
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-05-13 10:40:30 +0900

    Adjust styles

--- src/components/docsComponents/ArrowDescription.js
+++ src/components/docsComponents/ArrowDescription.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Box, Icon, Stack, Text } from '@chakra-ui/core';
+import { Box, Icon, Text } from '@chakra-ui/core';
 import { TiArrowRightOutline } from 'react-icons/ti';
 
 const ArrowDescription = ({ children: [title, description], ...props }) => (
   <Box py='5' {...props}>
     <Text fontWeight='bold'>{title}</Text>
-    <Stack isInline spacing='2' mt='2' align='center'>
-      <Icon as={TiArrowRightOutline} fontSize='xl' />
-      <Text opacity='.9'>{description}</Text>
-    </Stack>
+    <Box mt='2' position='relative' opacity='.9'>
+      <Icon
+        as={TiArrowRightOutline}
+        fontSize={['md', 'xl']}
+        position='absolute'
+        top={['15%', '10%']}
+      />
+      <Text ml={[5, 8]}>{description}</Text>
+    </Box>
   </Box>
 );
 
