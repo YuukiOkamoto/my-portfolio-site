7654f26c660d849bee5f7752496ff0daa3f2d66a
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-05-25 11:48:36 +0900

    Fix widget

--- src/components/widgetComponents/FigureImage.js
+++ src/components/widgetComponents/FigureImage.js
@@ -3,7 +3,7 @@ import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import { Box } from '@chakra-ui/core'
 
-const FigureImage = ({ src, alt, caption, ...props }) => {
+const FigureImage = ({ src, alt, children, ...props }) => {
   const { allFile } = useStaticQuery(graphql`
     {
       allFile {
@@ -28,7 +28,9 @@ const FigureImage = ({ src, alt, caption, ...props }) => {
   return (
     <Box as='figure' w={['full', '70%']} mx='auto' my='4' {...props}>
       <Img fluid={image.node.childImageSharp.fluid} alt={alt} />
-      <Box as='figcaption' textAlign='center' color='gray.700' mt='2'>{caption}</Box>
+      <Box as='figcaption' textAlign='center' color='gray.700' mt='2'>
+        {children}
+      </Box>
     </Box>
   );
 };
