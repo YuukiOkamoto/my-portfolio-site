2d05152eca2cc05ab7d1ce39c20e19adf271d4be
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-06-09 10:28:10 +0900

    Update Status styles

--- src/components/Ability/Status.js
+++ src/components/Ability/Status.js
@@ -4,18 +4,24 @@ import { useTheme, Flex, Text } from '@chakra-ui/core';
 import { generateAlphaColors } from '../../theme/colors-utils';
 
 const Grade = ({ value, ...props }) => {
+  const { colors } = useTheme();
+
   let grade = {};
-  if (value < 30) grade = { value: 'G', color: 'gray.500' };
-  else if (value < 40) grade = { value: 'F', color: 'cyan.500' };
-  else if (value < 50) grade = { value: 'E', color: 'green.400' };
-  else if (value < 60) grade = { value: 'D', color: 'yellow.200' };
-  else if (value < 70) grade = { value: 'C', color: 'yellow.500' };
-  else if (value < 80) grade = { value: 'B', color: 'red.500' };
-  else if (value < 100) grade = { value: 'A', color: 'pink.500' };
-  else grade = { value: 'S', color: 'pink.200' };
+  if (value < 30) grade = { value: 'G', color: colors.gray[500] };
+  else if (value < 40) grade = { value: 'F', color: colors.cyan[500] };
+  else if (value < 50) grade = { value: 'E', color: colors.green[400] };
+  else if (value < 60) grade = { value: 'D', color: colors.yellow[200] };
+  else if (value < 70) grade = { value: 'C', color: colors.yellow[500] };
+  else if (value < 80) grade = { value: 'B', color: colors.red[500] };
+  else if (value < 100) grade = { value: 'A', color: colors.pink[500] };
+  else grade = { value: 'S', color: colors.pink[200] };
 
   return (
-    <Text color={grade.color} {...props}>
+    <Text
+      color={grade.color}
+      textShadow={`0px 1px 2px ${grade.color}`}
+      {...props}
+    >
       {grade.value}
     </Text>
   );
@@ -30,8 +36,8 @@ const Status = ({ name, value, type, ...props }) => {
       justify='space-between'
       borderRadius='lg'
       bg='white'
-      maxW='200px'
-      py='2'
+      shadow='sm'
+      py='1'
       pl='2'
       pr='6'
       {...props}
@@ -46,12 +52,18 @@ const Status = ({ name, value, type, ...props }) => {
           generateAlphaColors(colors.gray[300])[400]
         } inset`}
         color='blue.600'
-        w='80px'
+        fontWeight='bold'
+        w='40%'
       >
         {name}
       </Flex>
-      <Grade value={value} fontSize='xl' fontWeight='bold' />
-      <Text color='blue.700' fontSize='xl' fontWeight='bold'>
+      <Grade value={value} fontSize='2xl' fontWeight='bold' />
+      <Text
+        color='blue.700'
+        fontSize='2xl'
+        fontWeight='bold'
+        textShadow={`0px 1px 2px ${colors.blue[[700]]}`}
+      >
         {value}
       </Text>
     </Flex>
