ad6d69780fd42e10654c5e08a5e4c0dcc26ccac0
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-06-18 15:47:15 +0900

    Update widgetComponents

--- src/components/widgetComponents/index.js
+++ src/components/widgetComponents/index.js
@@ -1,5 +1,17 @@
 import ArrowDescription from './ArrowDescription';
 import Callout from './Callout';
+import Collapse from './Collapse';
 import ExternalLink from './ExternalLink';
+import FigureImage from './FigureImage';
+import Link from './Link';
+import PostLink from './PostLink';
 
-export { ArrowDescription, Callout, ExternalLink };
+export {
+  ArrowDescription,
+  Callout,
+  Collapse,
+  ExternalLink,
+  FigureImage,
+  Link,
+  PostLink,
+};
