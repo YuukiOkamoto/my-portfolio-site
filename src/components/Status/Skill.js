b2783a0d900d2024794e02924509daf9defed0e0
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-06-18 11:35:25 +0900

    clean

--- src/components/Status/Skill.js
+++ src/components/Status/Skill.js
@@ -32,23 +32,6 @@ const Skill = ({ mainType, subType, children, ...props }) => {
         &::before {
           content: '';
           position: absolute;
-          top: 0;
-          width: 50%;
-          height: 40px;
-          left: 50%;
-          background: linear-gradient(
-            to bottom,
-            ${colors[subColor ? subColor : mainColor][100]} 0%,
-            ${colors[subColor ? subColor : mainColor][100]} 50%,
-            ${colors[subColor ? subColor : mainColor][200]} 50%,
-            ${colors[subColor ? subColor : mainColor][200]} 100%
-          );
-          z-index: 1;
-          border-radius: 0 8px 8px 0;
-        }
-        &::after {
-          content: '';
-          position: absolute;
           top: -1px;
           bottom: -1px;
           left: -1px;
@@ -62,6 +45,23 @@ const Skill = ({ mainType, subType, children, ...props }) => {
           );
           z-index: -1;
           border-radius: 8px;
+        }
+        &::after {
+          content: '';
+          position: absolute;
+          top: 0;
+          width: 50%;
+          height: 40px;
+          left: 50%;
+          background: linear-gradient(
+            to bottom,
+            ${colors[subColor ? subColor : mainColor][100]} 0%,
+            ${colors[subColor ? subColor : mainColor][100]} 50%,
+            ${colors[subColor ? subColor : mainColor][200]} 50%,
+            ${colors[subColor ? subColor : mainColor][200]} 100%
+          );
+          z-index: 1;
+          border-radius: 0 8px 8px 0;
         }
       `}
       {...props}
